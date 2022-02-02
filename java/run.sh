2a7f54a6eb8b5ab5bfeaf1180e2f4e019483a6d9
liuyuxiang512 <liuyuxiang512@gmail.com>
2022-02-01 22:03:57 -0600

    update mp3 to mp8

--- java/run.sh
+++ java/run.sh
@@ -2,10 +2,10 @@
 
 if [ -z "$1" ] 
 then 
-    echo "Usage: run.sh [MainClassName] e.g. MP3_PartA/B/C/D/F"
+    echo "Usage: run.sh [MainClassName] e.g. MP8_PartA/B/C/D/F"
     exit 1
 fi 
 
 mvn package
 
-spark-submit --class $1 target/mp3_sparksql-1.0.0-SNAPSHOT.jar 
+spark-submit --class $1 target/mp8_sparksql-1.0.0-SNAPSHOT.jar
